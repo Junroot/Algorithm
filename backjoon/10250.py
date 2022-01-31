b45093949ed9549d1ec8ba0a4ed70e843e76e6b6
ROOT <junroot0909@gmail.com>
2022-01-31 15:23:07 +0900

    refactor: 220129 문제 풀이 리팩터링

--- backjoon/10250.py
+++ backjoon/10250.py
@@ -6,8 +6,5 @@ for _ in range(t):
     h, w, n = map(int, sys.stdin.readline().strip().split())
     x = (n - 1) // h + 1
     y = (n - 1) % h + 1
-    print(y, end="")
-    if x < 10:
-        print("0" + str(x))
-    else:
-        print(x)
+    print(y * 100 + x)
+
